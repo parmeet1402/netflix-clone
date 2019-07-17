5bb9ec46e975fe02d34f07e85b1d71a631bace19
parmeet.singh@creditculture.sg-at-815337547111 <parmeet.singh@creditculture.sg>
2019-07-17 15:45:29 +0530

    search field added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import "./App.css"
+export default class App extends Component {
+  constructor(props){
+  super(props);
+    this.state = {
+      searchQuery: "",
+      searchUrl: ""
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onKeyUp = e => {
+    if(e.key === "Enter" && this.state.searchQuery !== ""){
+      let searchUrl = `search/multi?query=${this.state.searchQuery}&api_key=${this.apiKey}`;
+      this.setState({
+        searchUrl
+      })
+    }
+  }
+  render() {
+    return (
+      <div>
+        <input name="searchQuery" value={this.state.searchQuery} onKeyUp={this.onKeyUp} placeholder="Search for movies" size="40" onChange={this.onChange} type="text"/>
+        {/* TITLE LIST COMPONENT */}
+        {/* LIST TOGGLE COMPONENT */}
+      </div>
+    )
+  }
 }
-
-export default App;
